75348ced2508958b73e78f1c088b9d89de76f49c
Valahaar <campolungo@di.uniroma1.it>
2021-06-12 12:59:27 +0200

    auto(un)set pythonpath

--- setup/showcase-autoenv.sh
+++ setup/showcase-autoenv.sh
@@ -1,4 +1,6 @@
 mkdir test-autoenv
 conda create -y -n autoenv python=3.9
 echo 'conda activate autoenv' > test-autoenv/.in
+echo 'PYTHONPATH="$(dirname $0)/src"' >> test-autoenv/.in
 echo 'conda deactivate' > test-autoenv/.out
+echo 'unset PYTHONPATH' >> test-autoenv/.out
